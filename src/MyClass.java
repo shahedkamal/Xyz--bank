b84b6a76c383e1e293856cf4d13aefb343471a03
shahedkamal <shahedmorshed99@gmail.com>
2023-01-09 12:29:53 +0300

    update

---
+++ src/MyClass.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver;
+		
+
+		WebDriverManager.chromedriver().setup();
+
+		driver = new ChromeDriver();
+		driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
+
+	}
+
+}
