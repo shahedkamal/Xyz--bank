51b9215ddc635cf4cefb23651d76db9e78e7ef9a
shahedkamal <shahedmorshed99@gmail.com>
2023-01-09 15:00:19 +0300

    this is screenshot process

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,6 +1,12 @@
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Date;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,7 +15,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class MyClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		WebDriver driver;
 		
@@ -37,6 +43,28 @@ public class MyClass {
 		WebElement myInputForTheFirstName = driver
 				.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[1]/input"));
 		myInputForTheFirstName.sendKeys(myListOfFirstName[randomOne]);
+
+		WebElement myInputForTheLastName = driver
+				.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[2]/input"));
+
+		myInputForTheLastName.sendKeys(myListOfLastName[randomTwo]);
+
+		int PostalCodeRandomNumber = (int) (Math.random() * 10000);
+		String PostalCodeString = Integer.toString(PostalCodeRandomNumber);
+		driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[3]/input"))
+		.sendKeys(PostalCodeString);
+		Date currentTime = new Date();
+
+		String myUpdatedate = currentTime.toString();
+
+		String NewDate = myUpdatedate.replace(":", "-");
+
+
+
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File file = ts.getScreenshotAs(OutputType.FILE);
+
+		FileUtils.copyFile(file, new File("./ScreenShot_Folder/" + NewDate + ".jpg"));
 
 	}
 
